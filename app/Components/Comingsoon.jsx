37b0c395852f3add4f9d5ab26c6755280220c918
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2025-01-16 08:44:41 +0530

    scrool fix

--- app/Components/Comingsoon.jsx
+++ app/Components/Comingsoon.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 const UnderConstruction = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div className="text-center mt-[-50px]  p-8 max-w-md">
+    <div className="flex items-center justify-center h-screen bg-black overflow-hidden">
+      <div className="text-center p-8 max-w-md">
         <img
-        width={250} height={250}
+          width={250}
+          height={250}
           src="./alhamdlogo.png" // Replace with your client's logo URL
           alt="Company Logo"
-          className="ml-10 md:ml-16"
+          className="mx-auto mb-[-50px] "
         />
-        <h1 className="text-3xl mt-[-50px] font-bold text-white">
+        <h1 className="text-3xl font-bold text-white mt-4">
           COMING SOON
         </h1>
-        <p className="text-white mb-6">
+        <p className="text-white mb-6 mt-2">
           We’re currently working on something exciting! Please check back soon.
         </p>
         <p className="text-gray-500 text-sm">
