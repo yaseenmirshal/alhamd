219ec219c6c16ad8b958791c91e28a3099a9a7ea
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2025-01-16 08:47:32 +0530

    replaced back the code

--- app/Components/Comingsoon.jsx
+++ app/Components/Comingsoon.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 const UnderConstruction = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-black overflow-hidden">
-      <div className="text-center p-8 max-w-md">
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="text-center mt-[-50px]  p-8 max-w-md">
         <img
-          width={250}
-          height={250}
+        width={250} height={250}
           src="./alhamdlogo.png" // Replace with your client's logo URL
           alt="Company Logo"
-          className="mx-auto mb-[-50px] "
+          className="ml-10 md:ml-16"
         />
-        <h1 className="text-3xl font-bold text-white mt-4">
+        <h1 className="text-3xl mt-[-50px] font-bold text-white">
           COMING SOON
         </h1>
-        <p className="text-white mb-6 mt-2">
+        <p className="text-white mb-6">
           We’re currently working on something exciting! Please check back soon.
         </p>
         <p className="text-gray-500 text-sm">
@@ -23,7 +22,7 @@ const UnderConstruction = () => {
             href="mailto:alhamdtraders@gmail.com"
             className="text-blue-600 hover:underline"
           >
-            alhamdtarders@gmail.com
+            alhamdtarder@gmail.com
           </a>
         </p>
       </div>
